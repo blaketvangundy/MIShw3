7f191a1b0acc32a2eb42966d747e67b7eac0ecfd
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-10-02 01:09:08 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-courses.php");
 
+$pageTitle = "Courses";
+include "view-header.php";
+$courses = selectCourses();
+include "view-instructors.php";
+include "view-footer.php";
+?>
