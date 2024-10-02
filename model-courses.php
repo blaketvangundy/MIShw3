698d024c36cc259422a348fb5bf0f26ff66a7453
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-10-02 01:11:14 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectCourses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM `homework#3`.course;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
