fa447d8ae507e6a8b36122555d7d0ed69b5c9b42
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-10-02 01:47:42 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -23,7 +23,7 @@
           <a class="nav-link" href="instructors.php">Instructors</a>
         </li>
                 <li class="nav-item">
-          <a class="nav-link" href="Courses.php">Coursest</a>
+          <a class="nav-link" href="Courses.php">Courses</a>
         </li>
                 <li class="nav-item">
           <a class="nav-link" href="instructors-with-courses.php">Instructors with courses</a>
