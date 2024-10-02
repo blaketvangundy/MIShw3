78f43bc8f63b4cc2f608b6a5b4a7660b8ac94ef0
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-10-02 00:25:29 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('129.15.65.247', 'blakevangundy4013', 'R@chel1Bl@ke2', 'mis-4013-blakevangundy-db.mysql.database.azure.com');
+    $conn = new mysqli('mis-4013-blakevangundy-db.mysql.database.azure.com', 'blakevangundy4013', 'R@chel1Bl@ke2', 'homework#3');
     
     // Check connection
     if ($conn->connect_error) {
