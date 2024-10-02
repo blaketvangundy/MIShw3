334990f791c9ae3f0a06af55498863f6790041a2
blaketvangundy <157446746+blaketvangundy@users.noreply.github.com>
2024-10-02 01:23:14 -0500

    Update courses.php

--- courses.php
+++ courses.php
@@ -5,6 +5,6 @@ require_once("model-courses.php");
 $pageTitle = "Courses";
 include "view-header.php";
 $courses = selectCourses();
-include "view-instructors.php";
+include "view-courses.php";
 include "view-footer.php";
 ?>
